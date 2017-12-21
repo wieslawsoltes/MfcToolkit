2eb5b6be3245c852c8bf3479130b68d37ed87c52
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-12-21 18:07:44 +0100

    Added CMySpinButtonCtrl class

---
+++ controls/MySpinButtonCtrl.cpp
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include "StdAfx.h"
+#include "MySpinButtonCtrl.h"
+
+IMPLEMENT_DYNAMIC(CMySpinButtonCtrl, CSpinButtonCtrl)
+CMySpinButtonCtrl::CMySpinButtonCtrl()
+{
+
+}
+
+CMySpinButtonCtrl::~CMySpinButtonCtrl()
+{
+
+}
+
+BEGIN_MESSAGE_MAP(CMySpinButtonCtrl, CSpinButtonCtrl)
+END_MESSAGE_MAP()
---
+++ controls/MySpinButtonCtrl.h
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#pragma once
+
+class CMySpinButtonCtrl : public CSpinButtonCtrl
+{
+    DECLARE_DYNAMIC(CMySpinButtonCtrl)
+public:
+    CMySpinButtonCtrl();
+    virtual ~CMySpinButtonCtrl();
+protected:
+    DECLARE_MESSAGE_MAP()
+};
