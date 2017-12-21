d2bd53bc101b80ef6e9fe2df95572af5fe4e35e0
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-12-21 18:04:31 +0100

    Added CMyProgressCtrl class

---
+++ controls/MyProgressCtrl.cpp
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include "StdAfx.h"
+#include "MyProgressCtrl.h"
+
+IMPLEMENT_DYNAMIC(CMyProgressCtrl, CProgressCtrl)
+CMyProgressCtrl::CMyProgressCtrl()
+{
+
+}
+
+CMyProgressCtrl::~CMyProgressCtrl()
+{
+
+}
+
+BEGIN_MESSAGE_MAP(CMyProgressCtrl, CProgressCtrl)
+END_MESSAGE_MAP()
