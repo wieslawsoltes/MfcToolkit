2a7598073c7ac40ec4a4a816144c735ad4f5545c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-01-23 18:07:10 +0100

    Optimize convert

--- utilities/Utf8String.h
+++ utilities/Utf8String.h
@@ -11,24 +11,24 @@ namespace util
     class CUtf8String
     {
     public:
-        static const std::string ConvertToUtf8(const std::wstring& szText)
+        static std::string ConvertToUtf8(const std::wstring& szText)
         {
-            std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> convert;
+            static std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> convert;
             return convert.to_bytes(szText);
         }
-        static const std::wstring ConvertToUnicode(const std::string& szText)
+        static std::wstring ConvertToUnicode(const std::string& szText)
         {
-            std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> convert;
+            static std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> convert;
             return convert.from_bytes(szText);
         }
-        static const std::string ConvertToUtf8(const wchar_t* szText)
+        static std::string ConvertToUtf8(const wchar_t* szText)
         {
-            std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> convert;
+            static std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> convert;
             return convert.to_bytes(szText);
         }
-        static const std::wstring ConvertToUnicode(const char* szText)
+        static std::wstring ConvertToUnicode(const char* szText)
         {
-            std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> convert;
+            static std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> convert;
             return convert.from_bytes(szText);
         }
     };
