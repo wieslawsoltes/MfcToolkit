bf5fe64cc8306ecac37f45313115230164d308b9
wieslawsoltes <wieslaw.soltes@gmail.com>
2018-02-07 07:42:00 +0000

    Add trim functions

--- utilities/StringHelper.h
+++ utilities/StringHelper.h
@@ -4,6 +4,8 @@
 #pragma once
 
 #include <algorithm>
+#include <cctype>
+#include <locale>
 #include <string>
 #include <sstream>
 #include <vector>
@@ -14,18 +16,18 @@ namespace util
     class StringHelper
     {
     public:
-        static std::wstring Convert(const std::string& str)
+        static inline std::wstring Convert(const std::string& str)
         {
             std::wstring w(str.begin(), str.end());
             return w;
         }
-        static std::string Convert(const std::wstring& str)
+        static inline std::string Convert(const std::wstring& str)
         {
             std::string s(str.begin(), str.end());
             return s;
         }
     public:
-        static std::string ToStringHex(int i)
+        static inline std::string ToStringHex(int i)
         {
             const char fill = '0';
             std::ostringstream stream;
@@ -34,15 +36,15 @@ namespace util
                 << std::hex << i;
             return stream.str();
         }
-        static int ToIntFromHex(const std::string& str)
+        static inline int ToIntFromHex(const std::string& str)
         {
             return std::stoi(str, nullptr, 16);
         }
-        static int ToInt(const std::string& str)
+        static inline int ToInt(const std::string& str)
         {
             return std::stoi(str);
         }
-        static std::vector<std::string> Split(const char *str, char c)
+        static inline std::vector<std::string> Split(const char *str, char c)
         {
             std::vector<std::string> result;
             do
@@ -54,25 +56,25 @@ namespace util
             } while (0 != *str++);
             return result;
         }
-        static std::string TowLower(const std::string& str)
+        static inline std::string TowLower(const std::string& str)
         {
             std::string s = str;
             std::transform(s.begin(), s.end(), s.begin(), ::towlower);
             return s;
         }
-        static std::string ToUpper(const std::string& str)
+        static inline std::string ToUpper(const std::string& str)
         {
             std::string s = str;
             std::transform(s.begin(), s.end(), s.begin(), ::toupper);
             return s;
         }
-        static bool CompareNoCase(const std::string& str1, const std::string& str2)
+        static inline bool CompareNoCase(const std::string& str1, const std::string& str2)
         {
             std::string s1 = ToUpper(str1);
             std::string s2 = ToUpper(str2);
             return s1 == s2;
         }
-        static bool ContainsNoCase(const std::string& str, const std::string& value, wchar_t token)
+        static inline bool ContainsNoCase(const std::string& str, const std::string& value, wchar_t token)
         {
             std::string v = ToUpper(value);
             auto tokens = Split(str.c_str(), token);
@@ -83,7 +85,7 @@ namespace util
             }
             return false;
         }
-        static bool ReplaceNoCase(std::string& str, const std::string& from, const std::string& to)
+        static inline bool ReplaceNoCase(std::string& str, const std::string& from, const std::string& to)
         {
             std::string s = ToUpper(str);
             std::string f = ToUpper(from);
@@ -95,14 +97,48 @@ namespace util
             }
             return false;
         }
-        static int FindNoCase(const std::string& str, const std::string& find)
+        static inline int FindNoCase(const std::string& str, const std::string& find)
         {
             std::string s = ToUpper(str);
             std::string f = ToUpper(find);
             return s.find(f);
         }
+        static inline void TrimLeft(std::string &s)
+        {
+            s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
+                return !std::isspace(ch);
+            }));
+        }
+        static inline void TrimRight(std::string &s)
+        {
+            s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+                return !std::isspace(ch);
+            }).base(), s.end());
+        }
+        static inline void Trim(std::string &s)
+        {
+            TrimLeft(s);
+            TrimRight(s);
+        }
+        static inline void TrimLeft(std::string &s, int c)
+        {
+            s.erase(s.begin(), std::find_if(s.begin(), s.end(), [c](int ch) {
+                return !(c == ch);
+            }));
+        }
+        static inline void TrimRight(std::string &s, int c)
+        {
+            s.erase(std::find_if(s.rbegin(), s.rend(), [c](int ch) {
+                return !(c == ch);
+            }).base(), s.end());
+        }
+        static inline void Trim(std::string &s, int c)
+        {
+            TrimLeft(s, c);
+            TrimRight(s, c);
+        }
     public:
-        static std::wstring ToWStringHex(int i)
+        static inline std::wstring ToWStringHex(int i)
         {
             const wchar_t fill = '0';
             std::wostringstream stream;
@@ -111,15 +147,15 @@ namespace util
                    << std::hex << i;
           return stream.str();
         }
-        static int ToIntFromHex(const std::wstring& str)
+        static inline int ToIntFromHex(const std::wstring& str)
         {
             return std::stoi(str, nullptr, 16);
         }
-        static int ToInt(const std::wstring& str)
+        static inline int ToInt(const std::wstring& str)
         {
             return std::stoi(str);
         }
-        static std::vector<std::wstring> Split(const wchar_t *str, wchar_t c)
+        static inline std::vector<std::wstring> Split(const wchar_t *str, wchar_t c)
         {
             std::vector<std::wstring> result;
             do
@@ -131,25 +167,25 @@ namespace util
             } while (0 != *str++);
             return result;
         }
-        static std::wstring TowLower(const std::wstring& str)
+        static inline std::wstring TowLower(const std::wstring& str)
         {
             std::wstring s = str;
             std::transform(s.begin(), s.end(), s.begin(), ::towlower);
             return s;
         }
-        static std::wstring ToUpper(const std::wstring& str)
+        static inline std::wstring ToUpper(const std::wstring& str)
         {
             std::wstring s = str;
             std::transform(s.begin(), s.end(), s.begin(), ::toupper);
             return s;
         }
-        static bool CompareNoCase(const std::wstring& str1, const std::wstring& str2)
+        static inline bool CompareNoCase(const std::wstring& str1, const std::wstring& str2)
         {
             std::wstring s1 = ToUpper(str1);
             std::wstring s2 = ToUpper(str2);
             return s1 == s2;
         }
-        static bool ContainsNoCase(const std::wstring& str, const std::wstring& value, wchar_t token)
+        static inline bool ContainsNoCase(const std::wstring& str, const std::wstring& value, wchar_t token)
         {
             std::wstring v = ToUpper(value);
             auto tokens = Split(str.c_str(), token);
@@ -160,7 +196,7 @@ namespace util
             }
             return false;
         }
-        static bool ReplaceNoCase(std::wstring& str, const std::wstring& from, const std::wstring& to)
+        static inline bool ReplaceNoCase(std::wstring& str, const std::wstring& from, const std::wstring& to)
         {
             std::wstring s = ToUpper(str);
             std::wstring f = ToUpper(from);
@@ -172,11 +208,45 @@ namespace util
             }
             return false;
         }
-        static int FindNoCase(const std::wstring& str, const std::wstring& find)
+        static inline int FindNoCase(const std::wstring& str, const std::wstring& find)
         {
             std::wstring s = ToUpper(str);
             std::wstring f = ToUpper(find);
             return s.find(f);
+        }
+        static inline void TrimLeft(std::wstring &s)
+        {
+            s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
+                return !std::isspace(ch);
+            }));
+        }
+        static inline void TrimRight(std::wstring &s)
+        {
+            s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+                return !std::isspace(ch);
+            }).base(), s.end());
+        }
+        static inline void Trim(std::wstring &s)
+        {
+            TrimLeft(s);
+            TrimRight(s);
+        }
+        static inline void TrimLeft(std::wstring &s, int c)
+        {
+            s.erase(s.begin(), std::find_if(s.begin(), s.end(), [c](int ch) {
+                return !(c == ch);
+            }));
+        }
+        static inline void TrimRight(std::wstring &s, int c)
+        {
+            s.erase(std::find_if(s.rbegin(), s.rend(), [c](int ch) {
+                return !(c == ch);
+            }).base(), s.end());
+        }
+        static inline void Trim(std::wstring &s, int c)
+        {
+            TrimLeft(s, c);
+            TrimRight(s, c);
         }
     };
 }
