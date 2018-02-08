57feca0f317d38a06ab8b7358077f6fd675513a8
wieslawsoltes <wieslaw.soltes@gmail.com>
2018-02-08 12:47:02 +0000

    Added missing file

---
+++ tests/MFC.Utilities.UnitTests/Strings.StringHelper.cpp
@@ -0,0 +1,25 @@
+﻿#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "utilities\StringHelper.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace MFCUtilitiesUnitTests
+{		
+    TEST_CLASS(StringHelper_Tests)
+    {
+    public:
+        TEST_METHOD(StringHelper_Convert_To_wstring)
+        {
+            std::string str = "Test string.";
+            std::wstring wstr = StringHelper::Convert(str);
+            Assert::AreEqual(L"Test string.", wstr);
+        }
+        TEST_METHOD(StringHelper_Convert_To_string)
+        {
+            std::string wstr = L"Test string.";
+            std::wstring str = StringHelper::Convert(wstr);
+            Assert::AreEqual("Test string.", str);
+        }
+    };
+}
