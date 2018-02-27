9305fa339c5eb0efd5540e6d3bf754dc08e6f621
wieslawsoltes <wieslaw.soltes@gmail.com>
2018-02-27 14:10:40 +0000

    Fix build errors

--- utilities/LanguageHelper.h
+++ utilities/LanguageHelper.h
@@ -3,11 +3,13 @@
 
 #pragma once
 
-#include <string>
+#define _AFXDLL
 #include <afxwin.h>
 #ifndef _AFX_NO_AFXCMN_SUPPORT
 #include <afxcmn.h>
 #endif
+
+#include <string>
 #include "LanguageContext.h"
 
 namespace lang
