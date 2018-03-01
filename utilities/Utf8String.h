a8ea6633002513619a6395c8d3d292135988f96f
wieslawsoltes <wieslaw.soltes@gmail.com>
2018-03-01 13:10:12 +0000

    Use std::move

--- utilities/Utf8String.h
+++ utilities/Utf8String.h
@@ -7,6 +7,7 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 #include <string>
+#include <utility>
 #else
 #include <locale>
 #include <codecvt>
@@ -25,7 +26,7 @@ namespace util
             int size_needed = WideCharToMultiByte(CP_UTF8, 0, &szText[0], (int)szText.size(), NULL, 0, NULL, NULL);
             std::string strTo(size_needed, 0);
             WideCharToMultiByte(CP_UTF8, 0, &szText[0], (int)szText.size(), &strTo[0], size_needed, NULL, NULL);
-            return strTo;
+            return std::move(strTo);
         }
         static std::string ToUtf8(const wchar_t* szText)
         {
@@ -34,7 +35,7 @@ namespace util
             int size_needed = WideCharToMultiByte(CP_UTF8, 0, &szText[0], (int)wcslen(szText), NULL, 0, NULL, NULL);
             std::string strTo(size_needed, 0);
             WideCharToMultiByte(CP_UTF8, 0, &szText[0], (int)wcslen(szText), &strTo[0], size_needed, NULL, NULL);
-            return strTo;
+            return std::move(strTo);
         }
         static std::wstring ToUnicode(const std::string &szText)
         {
@@ -43,7 +44,7 @@ namespace util
             int size_needed = MultiByteToWideChar(CP_UTF8, 0, &szText[0], (int)szText.size(), NULL, 0);
             std::wstring wstrTo(size_needed, 0);
             MultiByteToWideChar(CP_UTF8, 0, &szText[0], (int)szText.size(), &wstrTo[0], size_needed);
-            return wstrTo;
+            return std::move(wstrTo);
         }
         static std::wstring ToUnicode(const char* szText)
         {
@@ -52,7 +53,7 @@ namespace util
             int size_needed = MultiByteToWideChar(CP_UTF8, 0, &szText[0], (int)strlen(szText), NULL, 0);
             std::wstring wstrTo(size_needed, 0);
             MultiByteToWideChar(CP_UTF8, 0, &szText[0], (int)strlen(szText), &wstrTo[0], size_needed);
-            return wstrTo;
+            return std::move(wstrTo);
         }
 #else
         static std::string ToUtf8(const std::wstring& szText)
