c59c70be5ef64db152ca13e3625ec05e2fa670cb
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-03-03 21:39:21 +0100

    Remove line

--- utilities/Log.h
+++ utilities/Log.h
@@ -8,7 +8,6 @@ namespace util
     {
     public:
         virtual ~ILog() { }
-    public:
         virtual bool Open() = 0;
         virtual void Close() = 0;
         virtual void Log(const std::wstring szMessage, const bool bNewLine = true) = 0;
