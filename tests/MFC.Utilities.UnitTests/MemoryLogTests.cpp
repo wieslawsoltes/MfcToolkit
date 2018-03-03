a48a3e43ea851d32624ba45377c33ae204af202a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-03-03 21:35:40 +0100

    Added memory log

---
+++ tests/MFC.Utilities.UnitTests/MemoryLogTests.cpp
@@ -0,0 +1,36 @@
+﻿#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "utilities\MemoryLog.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace MFCUtilitiesUnitTests
+{
+    TEST_CLASS(MemoryLog_Tests)
+    {
+    public:
+        TEST_METHOD(MemoryLog_Constructor)
+        {
+            util::MemoryLog log;
+            Assert::AreEqual(size_t(0), log.m_Messages.size());
+        }
+
+        TEST_METHOD(MemoryLog_Message_NewLine)
+        {
+            util::MemoryLog log;
+            std::wstring szMessage = L"[Info] Log message";
+            log.Log(szMessage);
+            Assert::AreEqual(size_t(1), log.m_Messages.size());
+            Assert::AreEqual(szMessage + L"\n", log.m_Messages[0]);
+        }
+
+        TEST_METHOD(MemoryLog_Message_No_NewLine)
+        {
+            util::MemoryLog log;
+            std::wstring szMessage = L"[Info] Log message";
+            log.Log(szMessage, false);
+            Assert::AreEqual(size_t(1), log.m_Messages.size());
+            Assert::AreEqual(szMessage, log.m_Messages[0]);
+        }
+    };
+}
---
+++ utilities/MemoryLog.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+#include <string>
+#include <vector>
+#include "Log.h"
+
+namespace util
+{
+    class MemoryLog : public ILog
+    {
+    public:
+        std::vector<std::wstring> m_Messages;
+    public:
+        MemoryLog()
+        {
+        };
+        virtual ~MemoryLog()
+        {
+            Close();
+        }
+    public:
+        bool Open()
+        {
+            return true;
+        }
+        void Close()
+        {
+        }
+        void Log(const std::wstring szMessage, const bool bNewLine = true)
+        {
+            try
+            {
+                std::wstring szData = szMessage;
+                if (bNewLine)
+                    szData += L"\n";
+                m_Messages.emplace_back(szData);
+            }
+            catch (...) {}
+        }
+    };
+}
