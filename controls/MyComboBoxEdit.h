626ec7ef86c2388d45b3961c3c540769a241be87
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-09-22 20:27:57 +0200

    Updated headers

--- controls/MyComboBoxEdit.h
+++ controls/MyComboBoxEdit.h
@@ -1,7 +1,4 @@
-﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-#pragma once
+﻿#pragma once
 
 #define WM_MY_EN_CHANGE WM_USER + 0x20
 
